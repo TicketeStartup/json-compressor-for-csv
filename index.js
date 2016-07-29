e763d9944fd829afebbe2d96e6db722ec6798b22
jay <andrea-giglio@live.it>
2016-07-29 15:28:20 +0200

    Added unwind option

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 var compatted = [],
     lables = [];
+var _splitter = "-";
+var _unwind = true;
+
 
 function compatct(arrayJsonObjs) {
     var partials = [];
@@ -11,23 +14,28 @@ function compatct(arrayJsonObjs) {
         // find for not Array 
         for (var lbl in jsonObj) {
             lables.push(lbl);
-            if (!Array.isArray(jsonObj[lbl])) {
-                //console.log("Value: " + lbl);
+            //console.log("jsonObj[lbl]", jsonObj[lbl]);
+            //console.log("[lbl]", [lbl]);
+            //            console.log("constructor", jsonObj[lbl].constructor);
+            if (jsonObj[lbl] != undefined && !Array.isArray(jsonObj[lbl]) && jsonObj[lbl].constructor !== Object) {
+                //console.log("notNestedArrayParams:");
                 notNestedArrayParams.push(lbl);
             } else {
+                // console.log("partialsPush");
                 partialsPush = true;
             }
         }
         if (!partialsPush) {
-            //console.log("!partialsPush");
+            // console.log("!partialsPush");
             partials.push(getFromNestedArray(arrayJsonObjs[i], notNestedArrayParams));
         } else {
             //console.log("notNestedArrayParams", notNestedArrayParams);
             // find for Array nested
             var NestedArrayParams = {};
+            var partial = getFromNestedArray(arrayJsonObjs[i], notNestedArrayParams);
             for (var lbl in jsonObj) {
                 partialsPush = false;
-                if (jsonObj[lbl].constructor === Object) {
+                if (jsonObj[lbl] && jsonObj[lbl].constructor === Object) {
                     jsonObj[lbl] = new Array(jsonObj[lbl])
                 }
                 if (Array.isArray(jsonObj[lbl])) {
@@ -35,49 +43,73 @@ function compatct(arrayJsonObjs) {
                     //console.log("is array: " + Array.isArray(jsonObj[lbl]));
                     //console.log("Partial:" + Array.isArray(jsonObj[lbl]));
                     for (var sub_doc_index = 0; sub_doc_index < jsonObj[lbl].length; sub_doc_index++) {
-                        var partial = getFromNestedArray(arrayJsonObjs[i], notNestedArrayParams);
-                        for (sub_doc_2 in jsonObj[lbl][sub_doc_index]) {
-                            if (jsonObj[lbl][sub_doc_index][sub_doc_2].constructor === Object) {
-                                jsonObj[lbl][sub_doc_index][sub_doc_2] = new Array(jsonObj[lbl][sub_doc_index][sub_doc_2])
-                            }
-                            partial[lbl + "-" + sub_doc_2] = jsonObj[lbl][sub_doc_index][sub_doc_2];
-                            NestedArrayParams[lbl + "-" + sub_doc_2] = true;
+                        if (!_unwind) {
+                            partial = getFromNestedArray(arrayJsonObjs[i], notNestedArrayParams); // 2
                         }
-                        partials.push(partial);
-                        partialsPush == true;
+                        for (sub_doc_2 in jsonObj[lbl][sub_doc_index]) {
+                            if (jsonObj[lbl][sub_doc_index]) {
+                                if (jsonObj[lbl][sub_doc_index][sub_doc_2] && jsonObj[lbl][sub_doc_index][sub_doc_2].constructor === Object) {
+                                    jsonObj[lbl][sub_doc_index][sub_doc_2] = new Array(jsonObj[lbl][sub_doc_index][sub_doc_2])
+                                }
+                            }
+                            if (jsonObj[lbl][sub_doc_index]) {
+                                partial[lbl + _splitter + sub_doc_2] = jsonObj[lbl][sub_doc_index][sub_doc_2];
+                            }
+                            NestedArrayParams[lbl + _splitter + sub_doc_2] = true;
+                        }
+                        if (!_unwind) {
+                            partials.push(partial); //2
+                            partialsPush == true; // 2}
+                        }
+
+                        // console.log("Partials: \n", partials);
                     }
-                    // console.log("Partials: \n", partials);
+                }
+                if (_unwind) {
+                    partials.push(partial);
+                    partialsPush == true;
                 }
             }
         }
-        // console.log("\n----|" + i)
+        var allParams = notNestedArrayParams;
+        for (var l in NestedArrayParams) {
+            allParams.push(l);
+        }
+        return {
+            again: foundOneArray,
+            partials: partials,
+            allParams: allParams
+        };
     }
-    var allParams = notNestedArrayParams;
-    for (var l in NestedArrayParams) {
-        allParams.push(l);
-    }
-    return {
-        again: foundOneArray,
-        partials: partials,
-        allParams: allParams
-    };
-}
+};
 
 function getFromNestedArray(jsonObj, notNestedArrayParams) {
     var obj = {};
     for (var i = 0; i < notNestedArrayParams.length; i++) {
         obj[notNestedArrayParams[i]] = jsonObj[notNestedArrayParams[i]];
     }
+    // console.log("getFromNestedArray", getFromNestedArray);
     return obj;
 }
+
 // Run
-function start(exmp, callback) {
-    var test = compatct(exmp);
+function start(array, callback) {
+    var test = compatct(array);
     if (test.again === true) {
-        start(test.partials,callback);
+        //return callback(test);
+        //console.log("PARTIAL \n", test);
+        start(test.partials, callback);
     } else {
         //console.log("RESULT \n", test);
         return callback(test);
     }
 }
-exports.compress = start;
+
+function init(obj, callback) {
+    // console.log("obj", obj);
+    _unwind = obj.unwind !== undefined ? obj.unwind : true;
+    console.log("unwind: " + _unwind);
+    start(obj.list, callback);
+}
+
+exports.compress = init;
--- test/test.js
+++ test/test.js
@@ -1,87 +1,112 @@
-var compressor = require('./');
+var compressor = require('../');
 
-var exmp = [{
-       "firstName": "andrea",
-       "lastName": "giglio",
-       "games": [
-           {
-               "id": 0,
-               "code": "A"
-			},
-           {
-               "id": 1,
-               "code": "B"
-			},
-           {
-               "id": 2,
-               "code": "C"
-			},
-           {
-               "id": 0,
-               "code": "D",
-               "actions": [
-                   {
-                       "id": 0,
-                       "code": "A"
-                   },
-                   {
-                       "id": 21,
-                       "code": "J"
-                   },
-                   {
-                       "id": 32,
-                       "code": "K"
-                   },
-                   {
-                       "id": 0,
-                       "code": "A",
-                       "jays": [{
-                               "name": "COOL"
-                       }, {
-                               "name": "COOL1",
-                               "surname": "BU"
-                       },
-                           {
-                               "name": "COOL2"
-                       }]
-
-                   }
-				]
-			}
-	]
-},
-   {
-       "firstName": "luca",
-       "lastName": "medici",
-       "games": [
-           {
-               "id": 0,
-               "code": "E"
-			},
-           {
-               "id": 1,
-               "code": "F"
-			},
-           {
-               "id": 2,
-               "code": "G"
-			},
-           {
-               "id": 0,
-               "code": "H",
-               "actions": [
-                   {
-                       "id": 0,
-                       "code": "I"
-								}
-							]
-			}
-	]
+var exmp = [
+    {
+        "_id": "57766fc53543e84a003d4c43",
+        "user": [
+            {
+                "_id": "577554a89fcaae26c914a71c",
+                "email": "vefe2011@gmail.com",
+                "firstName": "Feliciana",
+                "lastName": "Savastano"
+                }
+        ],
+        "coupon": [
+            {
+                "used": [{
+                    "status": false
+                }],
+                "scratched": true,
+                "winning": true,
+                "analyzed": true,
+                "active": true,
+                "created": "2016-07-01T13:27:33.568Z",
+                "code": "952954743585",
+                "codeUsageBaseUrl": null
+}]
 }];
+//var exmp = [{
+//        "firstName": "andrea",
+//        "lastName": "giglio",
+//        "games": [
+//            {
+//                "id": 0,
+//                "code": "A"
+//			},
+//            {
+//                "id": 1,
+//                "code": "B"
+//			},
+//            {
+//                "id": 2,
+//                "code": "C"
+//			},
+//            {
+//                "id": 0,
+//                "code": "D",
+//                "actions": [
+//                    {
+//                        "id": 0,
+//                        "code": "A"
+//                   },
+//                    {
+//                        "id": 21,
+//                        "code": "J"
+//                   },
+//                    {
+//                        "id": 32,
+//                        "code": "K"
+//                   },
+//                    {
+//                        "id": 0,
+//                        "code": "A",
+//                        "jays": [{
+//                                "name": "COOL"
+//                       }, {
+//                                "name": "COOL1",
+//                                "surname": "BU"
+//                       },
+//                            {
+//                                "name": "COOL2"
+//                       }]
+//
+//                   }
+//				]
+//			}
+//	]
+//},
+//    {
+//        "firstName": "luca",
+//        "lastName": "medici",
+//        "games": [
+//            {
+//                "id": 0,
+//                "code": "E"
+//			},
+//            {
+//                "id": 1,
+//                "code": "F"
+//			},
+//            {
+//                "id": 2,
+//                "code": "G"
+//			},
+//            {
+//                "id": 0,
+//                "code": "H",
+//                "actions": [
+//                    {
+//                        "id": 0,
+//                        "code": "I"
+//								}
+//							]
+//			}
+//	]
+//}];
 
-
-compressor.compress(exmp,function(j){
-  console.log("Compressed " + JSON.stringify(j , 0 , 2));
+compressor.compress({
+    list: exmp,
+    unwind: true
+}, function (j) {
+    console.log("Compressed " + JSON.stringify(j, 0, 2));
 });
-
-
