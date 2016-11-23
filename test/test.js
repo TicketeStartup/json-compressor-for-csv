7d7521d2a05678c31c2388b65fe980f11bc1370f
jay <andrea-giglio@live.it>
2016-11-23 14:32:55 +0100

    Updated tests

--- test/test.js
+++ test/test.js
@@ -1,112 +1,170 @@
 var compressor = require('../');
 
-//var exmp = [{
-//    "firstName": "andrea",
-//    "lastName": "giglio",
-//    "games": [
-//        {
-//            "id": 0,
-//            "code": "A"
-//			},
-//        {
-//            "id": 1,
-//            "code": "B",
-//            "numer": 7,
-//            "actionsObj": {
-//                "id": 0,
-//                "code": "I",
-//                "numer": 8
-//            }
-//			},
-//        {
-//            "id": 2,
-//            "code": "C",
-//            "actions": [
-//                {
-//                    "id": 0,
-//                    "code": "I",
-//                    "numer": 8
-//				    },
-//                {
-//                    "id": 1,
-//                    "code": "J"
-//								}
-//							]
-//            }]
-//    },
-//    {
-//        "firstName": "luca",
-//        "lastName": "medici",
-//        "games": [
-//            {
-//                "id": 0,
-//                "code": "H",
-//                "actions": [
-//                    {
-//                        "id": 0,
-//                        "code": "I"
-//				    },
-//                    {
-//                        "id": 1,
-//                        "code": "J",
-//                        "books": [
-//                            {
-//                                "id": 0,
-//                                "code": "KK"
-//				    },
-//                            {
-//                                "id": 1,
-//                                "code": "LL"
-//				    }]
-//				    }]
-//            }]
-//    }];
-
-function ObjectId(a) {
-    return a
-};
-
-function ISODate(a) {
-    return a
-};
-
-var exmp = [ /* 1 */
-/* 1 */
-    {
-        "__v": 0,
-        "name": "andrea",
-        "deleted": {
-            "status": false,
-            "blabla": "wowo",
-            "cicio": {
-                "a": 1
-            }
-        },
-        "deleted2": [{
-            "status": false,
-            "bu" : 342
-//            "a": [{
-//                    "b": 1
-//            }, {
-//                    "c": 4
-//            }
-//                 ]
-        }, {
-            "status": true
-        }],
-        "lastAccess": [{
-            date: [ISODate("2016-08-19T20:21:25.833Z")]
-        }],
-        "changepasswordConfirmToken": ["NOT_REQUIRED", "REQUIRED"],
-        "changepasswordConfirmToken1": ["NOT_REQUIRED"]
-}
-];
-
+// TEST 2
+var exmp = [{
+    "firstName": "andrea",
+    "lastName": "giglio",
+    "games": [
+        {
+            "id": 0,
+            "code": "A"
+			}]
+}, {
+    "firstName": "andrea",
+    "lastName": "giglio",
+    "games": [
+        {
+            "id": 0,
+            "code": "A"
+			}]
+}];
 compressor.compress({
     list: exmp,
-    splitter: "-"
-}, function (j) {
+    splitter: "-",
+    splitResults: true
+}, function (result) {
     //console.log("Original " + JSON.stringify(exmp, 0, 2));
-    console.log("Compressed " + JSON.stringify(j, 0, 2));
-    // console.log("again " + j.again);
+    //  console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.partials[1]).length;
+    if (result && objectKeys === 4 && result.partials.length === 2) {
+        console.error("@--TEST 1.0                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 1.0                                           [ERROR] " + "");
+    }
+});
+compressor.compress({
+    list: exmp,
+    splitter: "-",
+    splitResults: false
+}, function (result) {
+    //console.log("Original " + JSON.stringify(exmp, 0, 2));
+    // console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.partials[1]).length;
+    if (result && objectKeys === 4 && result.partials.length === 2) {
+        console.error("@--TEST 1.1                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 1.1                                           [ERROR] " + "");
+    }
+});
+
+
+// TEST 2
+var exmp = [{
+    "firstName": "andrea",
+    "lastName": "giglio",
+    "games": [
+        {
+            "id": 0,
+            "code": "A"
+			}, {
+            "id": 1,
+            "code": "B"
+			}],
+    "games2": [
+        {
+            "id": 0,
+            "code": "A"
+			}, {
+            "id": 1,
+            "code": "B"
+			}],
+    "phone": "340177662",
+}];
+compressor.compress({
+    list: exmp,
+    splitter: "-",
+    splitResults: true
+}, function (result) {
+    //console.log("Original " + JSON.stringify(exmp, 0, 2));
+    //  console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.partials[0]).length;
+    if (result && objectKeys === 7 && result.partials.length === 4) {
+        console.error("@--TEST 2.1                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 2.1                                           [ERROR] " + "");
+    }
+});
+compressor.compress({
+    list: exmp,
+    splitter: "-",
+    splitResults: false
+}, function (result) {
+    //console.log("Original " + JSON.stringify(exmp, 0, 2));
+    //console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.partials[0]).length;
+    if (result && objectKeys === 11 && result.partials.length === 1) {
+        console.error("@--TEST 2.1                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 2.1                                           [ERROR] " + "");
+    }
+});
+
+// TEST 3
+var exmp = [{
+    "__v": 0,
+    "name": "andrea",
+    "deleted": {
+        "status": false,
+        "blabla": "wowo",
+        "cicio": {
+            "a": 1
+        }
+    },
+    "deleted2": [{
+            "status": false,
+            "bu": 342,
+            "a": [{
+                    "b": 1,
+                    "f": [
+                        {
+                            "h": 1,
+                            "h1": 1
+                        }, {
+                            "j": 3,
+                            "j2": 3,
+                            "j3": 3
+                    }
+                    ]
+            },
+                {
+                    "c": 4
+            }
+            ]
+    },
+        {
+            "status": true
+    }],
+    "lastAccess": [{
+        date: ["2016-08-19T20:21:25.833Z"]
+        }],
+    "changepasswordConfirmToken": ["NOT_REQUIRED", "REQUIRED"],
+    "changepasswordConfirmToken1": ["NOT_REQUIRED"]
+}];
+compressor.compress({
+    list: exmp,
+    splitter: "-",
+    splitResults: true
+}, function (result) {
+    //console.log("Original " + JSON.stringify(exmp, 0, 2));
+    //  console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.partials[0]).length;
+    if (result && objectKeys === 18 && result.partials.length === 16) {
+        console.error("@--TEST 3.0                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 3.0                                           [ERROR] " + "");
+    }
+});
+compressor.compress({
+    list: exmp,
+    splitter: "-",
+    splitResults: false
+}, function (result) {
+    // console.log("Original " + JSON.stringify(exmp, 0, 2));
+    //console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.partials[0]).length;
+    if (result && objectKeys === 19 && result.partials.length === 1) {
+        console.error("@--TEST 3.1                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 3.1                                           [ERROR] " + "");
+    }
 });
