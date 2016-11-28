a39b3424225efdc0f9dbb9ce5269a2847c8a2cf0
jay <andrea-giglio@live.it>
2016-11-28 16:31:20 +0100

    Fixed tests with one Exception case knowed

--- test/test.js
+++ test/test.js
@@ -1,6 +1,6 @@
 var compressor = require('../');
 
-// TEST 2
+// TEST 1
 var exmp = [{
     "firstName": "andrea",
     "lastName": "giglio",
@@ -20,33 +20,17 @@ var exmp = [{
 }];
 compressor.compress({
     list: exmp,
-    splitter: "-",
-    splitResults: true
+    splitter: "-"
 }, function (result) {
     //console.log("Original " + JSON.stringify(exmp, 0, 2));
-    //  console.log("Compressed " + JSON.stringify(result, 0, 2));
-    var objectKeys = Object.keys(result.partials[1]).length;
-    if (result && objectKeys === 4 && result.partials.length === 2) {
+    // console.log("Compressed " + JSON.stringify(result, 0, 2))
+    var objectKeys = Object.keys(result.results[1]).length;
+    if (result && objectKeys === 4 && result.results.length === 2 && result.labels.length === 4) {
         console.error("@--TEST 1.0                                           [OK] " + "");
     } else {
         console.error("@--TEST 1.0                                           [ERROR] " + "");
     }
 });
-compressor.compress({
-    list: exmp,
-    splitter: "-",
-    splitResults: false
-}, function (result) {
-    //console.log("Original " + JSON.stringify(exmp, 0, 2));
-    // console.log("Compressed " + JSON.stringify(result, 0, 2));
-    var objectKeys = Object.keys(result.partials[1]).length;
-    if (result && objectKeys === 4 && result.partials.length === 2) {
-        console.error("@--TEST 1.1                                           [OK] " + "");
-    } else {
-        console.error("@--TEST 1.1                                           [ERROR] " + "");
-    }
-});
-
 
 // TEST 2
 var exmp = [{
@@ -56,48 +40,38 @@ var exmp = [{
         {
             "id": 0,
             "code": "A"
-			}, {
+			},
+        {
             "id": 1,
             "code": "B"
-			}],
+			}
+    ],
     "games2": [
         {
             "id": 0,
             "code": "A"
-			}, {
+			},
+        {
             "id": 1,
             "code": "B"
-			}],
-    "phone": "340177662",
+			}
+    ],
+    "phone": "340177662"
 }];
 compressor.compress({
     list: exmp,
-    splitter: "-",
-    splitResults: true
+    splitter: "-"
 }, function (result) {
     //console.log("Original " + JSON.stringify(exmp, 0, 2));
-    //  console.log("Compressed " + JSON.stringify(result, 0, 2));
-    var objectKeys = Object.keys(result.partials[0]).length;
-    if (result && objectKeys === 7 && result.partials.length === 4) {
-        console.error("@--TEST 2.1                                           [OK] " + "");
+    // console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.results[0]).length;
+    if (result && objectKeys === 7 && result.results.length === 2 && result.labels.length === 7) {
+        console.error("@--TEST 2.0                                           [OK] " + "");
     } else {
-        console.error("@--TEST 2.1                                           [ERROR] " + "");
+        console.error("@--TEST 2.0                                           [ERROR] " + "");
     }
 });
-compressor.compress({
-    list: exmp,
-    splitter: "-",
-    splitResults: false
-}, function (result) {
-    //console.log("Original " + JSON.stringify(exmp, 0, 2));
-    //console.log("Compressed " + JSON.stringify(result, 0, 2));
-    var objectKeys = Object.keys(result.partials[0]).length;
-    if (result && objectKeys === 11 && result.partials.length === 1) {
-        console.error("@--TEST 2.1                                           [OK] " + "");
-    } else {
-        console.error("@--TEST 2.1                                           [ERROR] " + "");
-    }
-});
+
 
 // TEST 3
 var exmp = [{
@@ -110,61 +84,110 @@ var exmp = [{
             "a": 1
         }
     },
-    "deleted2": [{
-            "status": false,
-            "bu": 342,
-            "a": [{
-                    "b": 1,
-                    "f": [
-                        {
-                            "h": 1,
-                            "h1": 1
-                        }, {
-                            "j": 3,
-                            "j2": 3,
-                            "j3": 3
-                    }
-                    ]
-            },
-                {
-                    "c": 4
-            }
-            ]
-    },
-        {
-            "status": true
-    }],
     "lastAccess": [{
         date: ["2016-08-19T20:21:25.833Z"]
         }],
     "changepasswordConfirmToken": ["NOT_REQUIRED", "REQUIRED"],
-    "changepasswordConfirmToken1": ["NOT_REQUIRED"]
+    "changepasswordConfirmToken1": ["NOT_REQUIRED2"]
 }];
 compressor.compress({
     list: exmp,
-    splitter: "-",
-    splitResults: true
+    splitter: "-"
 }, function (result) {
     //console.log("Original " + JSON.stringify(exmp, 0, 2));
-    //  console.log("Compressed " + JSON.stringify(result, 0, 2));
-    var objectKeys = Object.keys(result.partials[0]).length;
-    if (result && objectKeys === 18 && result.partials.length === 16) {
+    // console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.results[0]).length;
+    if (result && objectKeys === 8 && result.results.length === 2 && result.labels.length === 8) {
         console.error("@--TEST 3.0                                           [OK] " + "");
     } else {
         console.error("@--TEST 3.0                                           [ERROR] " + "");
     }
 });
+
+// TEST 4
+var exmp = [{
+    "firstName": "andrea",
+    "lastName": "giglio",
+    "games": [
+        {
+            "id": 0,
+            "code": "A"
+		},
+        {
+            "id": 1,
+            "code": "B",
+            "numer": 7
+		},
+        {
+            "id": 2,
+            "code": "C",
+            "actions": [
+                {
+                    "id": 0,
+                    "code": "I",
+                    "numer": 8
+			    }
+						]
+        }]
+}];
+
 compressor.compress({
     list: exmp,
-    splitter: "-",
-    splitResults: false
+    splitter: "-"
 }, function (result) {
     // console.log("Original " + JSON.stringify(exmp, 0, 2));
-    //console.log("Compressed " + JSON.stringify(result, 0, 2));
-    var objectKeys = Object.keys(result.partials[0]).length;
-    if (result && objectKeys === 19 && result.partials.length === 1) {
-        console.error("@--TEST 3.1                                           [OK] " + "");
+    // console.log("Compressed " + JSON.stringify(result, 0, 2));
+    var objectKeys = Object.keys(result.results[2]).length;
+    if (result && objectKeys === 7 && result.results.length === 3 && result.labels.length === 8) {
+        console.error("@--TEST 4.0                                           [OK] " + "");
     } else {
-        console.error("@--TEST 3.1                                           [ERROR] " + "");
+        console.error("@--TEST 4.0                                           [ERROR] " + "");
+    }
+});
+
+// TEST 5
+var exmp = [{
+    "firstName": "andrea",
+    "lastName": "giglio",
+    "games": [
+        {
+            "id": 0,
+            "code": "A"
+		},
+        {
+            "id": 1,
+            "code": "B",
+            "numer": 7
+		},
+        {
+            "id": 2,
+            "code": "C",
+            "actions": [
+                {
+                    "id": 0,
+                    "code": "I",
+                    "numer": 8
+			    },
+                {
+                    "id": 1,
+                    "code": "IY",
+                    "numer": 88
+			    }
+				]
+        }]
+}];
+
+compressor.compress({
+    list: exmp,
+    splitter: "-"
+}, function (result) {
+    var objectKeys = Object.keys(result.results[0]).length;
+    if (result && objectKeys === 7 && result.results.length === 3 && result.labels.length === 8) {
+        console.error("@--TEST 5.0                                           [OK] " + "");
+    } else {
+        console.error("@--TEST 5.0                                           [ERROR] " + "");
+        console.error("      |--FIX TODO Missing object with More objects into Array after 2nd level" + "");
+        console.log("Original " + JSON.stringify(exmp, 0, 2));
+        console.log("Compressed: " + JSON.stringify(result, 0, 2));
     }
 });
